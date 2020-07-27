06bbee7f9e500f4cbb42c0d1a9b7a5bd8254413c
Goncalves Daniel <rgdaniel@indracompany.com>
2020-07-27 00:37:50 -0300

    changing the auth service from http to AngularFireAuth to handle authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { tap } from "rxjs/operators";
-import { Subject } from "rxjs";
+import { Subject, Observable } from "rxjs";
 import { User } from "../user/user.model";
+import { AngularFireAuth } from "angularfire2/auth";
 
 interface AuthResponseData{
     kind: string;
@@ -17,45 +18,78 @@ interface AuthResponseData{
 
 @Injectable({ providedIn: 'root', })
 export class AuthService{
-    user = new Subject<User>();
+    user: Observable<firebase.User>;
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private firebaseAuth: AngularFireAuth) {
+        this.user = firebaseAuth.authState;
+    }
 
     signUp(name, email: string, password: string){
-        return this.http.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCcAaqU4exlcXxOPnDjCmfmwhwbvioAgsg',
-            {
-                email: email,
-                password: password,
-                returnSecureToken: true
-            }
-        ).pipe(
-            tap(resData =>
-                {
-                    const expirationData = new Date(new Date().getTime() + +resData.expiresIn*1000);
-                    const user = new User(name, resData.email, resData.localId, resData.idToken,expirationData);
-                    this.user.next(user);
-                }
-            )
-        );
-    }
 
-    login(email: string, password: string){
-        return this.http.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCcAaqU4exlcXxOPnDjCmfmwhwbvioAgsg',
-            {
-                email: email,
-                password: password,
-                returnSecureToken: true
-            }
-        ).pipe(
-            tap(resData =>
-                {
-                    const expirationData = new Date(new Date().getTime() + +resData.expiresIn*1000);
-                    const user = new User(name, resData.email, resData.localId, resData.idToken,expirationData);
-                    this.user.next(user);
-                }
-            )
-        );;
-    }
+        this.firebaseAuth
+        .auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(value => {
+            console.log('Success!', value);
+        })
+        .catch(err => {
+            console.log('Something went wrong:',err.message);
+        });
+    } 
+    
+    login(email: string, password: string) {
+        this.firebaseAuth
+          .auth
+          .signInWithEmailAndPassword(email, password)
+          .then(value => {
+            console.log('Nice, it worked!');
+          })
+          .catch(err => {
+            console.log('Something went wrong:',err.message);
+          });
+      }
+
+      logout() {
+        this.firebaseAuth
+          .auth
+          .signOut();
+      }
+
+
+    //     return this.http.post<AuthResponseData>(
+    //         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCcAaqU4exlcXxOPnDjCmfmwhwbvioAgsg',
+    //         {
+    //             email: email,
+    //             password: password,
+    //             returnSecureToken: true
+    //         }
+    //     ).pipe(
+    //         tap(resData =>
+    //             {
+    //                 const expirationData = new Date(new Date().getTime() + +resData.expiresIn*1000);
+    //                 const user = new User(name, resData.email, resData.localId, resData.idToken,expirationData);
+    //                 this.user.next(user);
+    //             }
+    //         )
+    //     );
+    // }
+
+    // login(email: string, password: string){
+    //     return this.http.post<AuthResponseData>(
+    //         'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCcAaqU4exlcXxOPnDjCmfmwhwbvioAgsg',
+    //         {
+    //             email: email,
+    //             password: password,
+    //             returnSecureToken: true
+    //         }
+    //     ).pipe(
+    //         tap(resData =>
+    //             {
+    //                 const expirationData = new Date(new Date().getTime() + +resData.expiresIn*1000);
+    //                 const user = new User(name, resData.email, resData.localId, resData.idToken,expirationData);
+    //                 this.user.next(user);
+    //             }
+    //         )
+    //     );;
+    // }
 }
