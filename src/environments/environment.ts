410f21ab17bb4c92da07f429348cdfee254aace3
Goncalves Daniel <rgdaniel@indracompany.com>
2020-07-27 00:41:18 -0300

    Adding firebase credentials to the environment.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCcAaqU4exlcXxOPnDjCmfmwhwbvioAgsg",
+      authDomain: "sprint-planning-826b1.firebaseapp.com",
+      databaseURL: "https://sprint-planning-826b1.firebaseio.com",
+      projectId: "sprint-planning-826b1",
+      storageBucket: "sprint-planning-826b1.appspot.com",
+      messagingSenderId: "813687955540",
+      appId: "1:813687955540:web:151e480800413c5a5dcdc1",
+      measurementId: "G-QZ8SX9V9M5"
+  }
 };
 
 /*
