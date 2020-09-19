645e6275234926cf27dfc5a20a5326cc8881b872
Goncalves Daniel <rgdaniel@indracompany.com>
2020-09-19 00:21:47 -0300

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 // export const environment = {
 //   production: true
 // };
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyCcAaqU4exlcXxOPnDjCmfmwhwbvioAgsg",
+        authDomain: "sprint-planning-826b1.firebaseapp.com",
+        databaseURL: "https://sprint-planning-826b1.firebaseio.com",
+        projectId: "sprint-planning-826b1",
+        storageBucket: "sprint-planning-826b1.appspot.com",
+        messagingSenderId: "813687955540",
+        appId: "1:813687955540:web:151e480800413c5a5dcdc1",
+        measurementId: "G-QZ8SX9V9M5"
+    }
+  };
